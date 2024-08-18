d80838ea4b1761d8a49a28af080ded2b71cd7f48
H4iPh0ngM1enB4c <noobdownload2@gmail.com>
2024-08-18 22:38:41 +0700

    recode

--- src/main/java/meteordevelopment/meteorclient/events/render/ApplyTransformationEvent.java
+++ src/main/java/meteordevelopment/meteorclient/events/render/ApplyTransformationEvent.java
@@ -7,7 +7,6 @@ package meteordevelopment.meteorclient.events.render;
 
 import meteordevelopment.meteorclient.events.Cancellable;
 import net.minecraft.client.render.model.json.Transformation;
-import net.minecraft.client.util.math.MatrixStack;
 
 public class ApplyTransformationEvent extends Cancellable {
     private static final ApplyTransformationEvent INSTANCE = new ApplyTransformationEvent();
@@ -17,7 +16,7 @@ public class ApplyTransformationEvent extends Cancellable {
     public MatrixStack matrices;
 
     public static ApplyTransformationEvent get(Transformation transformation, boolean leftHanded, MatrixStack matrices) {
-        INSTANCE.setCancelled(false);
+        INSTANCE.setCancelled(true);
 
         INSTANCE.transformation = transformation;
         INSTANCE.leftHanded = leftHanded;
